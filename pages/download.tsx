f45d23acbe1207819c1c19f35619d8da849dcde5
pyshnenko <pyshnenko94@yandex.ru>
2024-01-24 11:12:50 +0300

    upd uploader

--- pages/download.tsx
+++ pages/download.tsx
@@ -17,7 +17,7 @@ export default function DownloadPage () {
         const name = params.get("name") as string;
         setfname(name);
         const type = params.get("type") === 'true';
-        if (!type) download_file(encodeURI(`http://localhost:8800/openLinc?tok=${tok}`), name)
+        if (!type) download_file(encodeURI(`/openLinc?tok=${tok}`), name)//http://localhost:8800
         setType(String(type));
     }, [])
 
