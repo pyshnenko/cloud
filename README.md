a12b01e6a6442a1ac107a883870ceb25b2090cdc
pyshnenko <pyshnenko94@yandex.ru>
2024-02-15 16:17:09 +0300

    add lines

--- README.md
+++ README.md
@@ -1,36 +1,21 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Облако
 
-## Getting Started
+Файлы хранятся на сервере БЕЗ ШИФРОВАНИЯ
+(это нужно для своих целей)
 
-First, run the development server:
+У каждого пользователя свое хранилище.
+Доступ по токену, без токена сервер не знает полный путь
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+Можно создавать папки, скачивать папки целиком архивом
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+Можно создавать открытые папки или давать сторонний доступ к отдельным файлам по уникальным ссылкам
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+Развернуто на next.ts, данные о пользователях и ключи в монге
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+https://cloud.spamigor.ru
 
-## Learn More
+##lines
+https://cloud.spamigor.ru/lines
 
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+Простенькая анимация на canvas. Главная особенность - синхронизация 2 вкладок через localStorage
+шарик вылетает из основной вкладки, влетает во вторую вкладку. При изменении позиции вкладки, направление динамически меняется
--- pages/lines.tsx
+++ pages/lines.tsx
@@ -7,12 +7,17 @@ export default function Lines () {
     const [screenX, setScreenX] = useState(0);
     const [screenY, setScreenY] = useState(0);
     const [angle, setAngle] = useState(0);
-    const [endPoint, setEndPoint] = useState({w: 0, h: 100});
+    const [endPoint, setEndPoint] = useState({w: 400, h: 700});
+    const [startPoint, setStartPoint] = useState({w: 400, h: 700});
     const [flightPoint, setFlightPoint] = useState({w: 0, h:0});
+    const [server, setServer] = useState(true);
     const trig1 = useRef(true);
     const screenXRef = useRef(screenX);
     const screenYRef = useRef(screenY);
     const lastAngle = useRef(0);
+    const endPointRef = useRef(endPoint);
+    const startPointRef = useRef(startPoint);
+    const serverRef = useRef(true);
 
     useEffect(() => {
         if (trig1.current) {
@@ -21,18 +26,57 @@ export default function Lines () {
             setHeight(window.innerHeight);
             setScreenX(window.screenX);
             setScreenY(window.screenY);
+            setAngle(Math.atan(((window.screenY+window.innerHeight/2)-endPoint.h)/((window.screenX+window.innerWidth/2)-endPoint.w))||0);
+            const serv = (localStorage.getItem('lineServer')!=='true')||((Number(new Date()) - Number((localStorage.getItem('lineTime')||0)))>2000)
+            console.log(serv)
+            setServer(serv)
+
 
 
             const handleMove = () => {
                 setScreenX(window.screenX);
                 setScreenY(window.screenY);
+                if (serv) {
+                    setAngle(Math.atan(((window.screenY+window.innerHeight/2)-endPointRef.current.h)/((window.screenX+window.innerWidth/2)-endPointRef.current.w))||0);
+                    setStartPoint({w: window.screenX+window.innerWidth/2, h: window.screenY+window.innerHeight/2})
+                }
+                localStorage.setItem(serv?'lineWStart':'lineWEnd', String(window.screenX+window.innerWidth/2))
+                localStorage.setItem(serv?'lineHStart':'lineHEnd', String(window.screenY+window.innerHeight/2))
             }
             const handleResize = (event: any) => {
                 setWidth(event.target.innerWidth);
                 setHeight(event.target.innerHeight);
+                if (serv) {
+                    setAngle(Math.atan(((window.screenY+window.innerHeight/2)-endPoint.h)/((window.screenX+window.innerWidth/2)-endPoint.w))||0);
+                }
+                localStorage.setItem(serv?'lineWStart':'lineWEnd', String(window.screenX+window.innerWidth/2))
+                localStorage.setItem(serv?'lineHStart':'lineHEnd', String(window.screenY+window.innerHeight/2))
             };
 
-            setInterval(()=>setFlightPoint({w:-17, h:0}), 3000);
+            const handleStorage = () => {
+                if (serv) {
+                    const dTime = Number(new Date()) - Number(localStorage.getItem('lineClientTime'))
+                    setEndPoint({w: dTime>10000 ? 0 : Number(localStorage.getItem('lineWEnd'))||0, h: dTime>10000 ? 0 : Number(localStorage.getItem('lineHEnd'))||0})
+                    console.log('storage: ' + serv);
+                }
+                else {
+                    setStartPoint({w: Number(localStorage.getItem('lineWStart')||0), h: Number(localStorage.getItem('lineHStart')||0)})
+                    if (localStorage.getItem('startLine')==='true') {
+                        setFlightPoint({w: startPointRef.current.w, h:startPointRef.current.h});
+                        localStorage.setItem('startLine', 'false')
+                    }
+                }
+            }
+
+            if (serv) {
+                setInterval(()=>localStorage.setItem('lineTime', String(Number(new Date()))), 1000);
+                localStorage.setItem('lineServer', 'true');            
+                setInterval(()=>{
+                    setFlightPoint({w: 0, h:0});
+                    localStorage.setItem('startLine', 'true');
+                }, 2000);
+            }
+            else setInterval(()=>localStorage.setItem('lineClientTime', String(Number(new Date()))), 1000);
 
             const timeOutFunction = () => {
                 if ((window.screenX!==screenXRef.current)||(window.screenY!==screenYRef.current)) {
@@ -49,6 +93,7 @@ export default function Lines () {
             //roatation(angle);
             window.addEventListener('resize', handleResize);
             window.addEventListener('mouseup', handleMove);
+            window.addEventListener('storage', handleStorage)
             timeOutFunction();
             return () => {
                 window.removeEventListener('resize', handleResize);
@@ -58,72 +103,67 @@ export default function Lines () {
     }, []);
 
     useEffect(()=>{
-        const x1 = window.screenX;
-        const y1 = window.screenY;
-        const aangle = Math.atan(((y1+window.innerHeight/2)-endPoint.h)/((x1+window.innerWidth/2)-endPoint.w))||0;
-        console.log(`W: ${width}, H: ${height}, X: ${screenX}, Y: ${screenY}, alpha: ${aangle*180/Math.PI}, last: ${lastAngle.current*180/Math.PI}`);
         screenXRef.current = screenX;
         screenYRef.current = screenY;
 
-        if (flightPoint.w>=-17 && flightPoint.w<1000) 
-            setTimeout((w: number, h: number)=>{
-                setFlightPoint({w: w+5, h: h})
-            }, 10, flightPoint.w, flightPoint.h)
-
+        if ((Math.abs((flightPoint.w+width/2+screenX)-endPoint.w)>9 && Math.abs((flightPoint.h+height/2 + screenY)-endPoint.h)>9)&&server)//(flightPoint.w<=1000 && flightPoint.w>-1000) && (flightPoint.h<=1000 && flightPoint.h>-1000))
+            setTimeout((w: number, h: number, sX: number, sY: number, wi: number, he: number, endP: {w: number, h: number}, ang: number)=>{
+                const ySpeed = -10*Math.sin(ang);
+                const xSpeed = 10*Math.cos(ang);
+                setFlightPoint({w: (wi/2+sX)>(endP.w)?w-xSpeed:w+xSpeed, h: (wi/2+sX)<(endP.w)?h-ySpeed:h+ySpeed})
+            }, 10, flightPoint.w, flightPoint.h, screenX, screenY, width, height, endPoint, angle)
+        else if ((Math.abs((flightPoint.w-screenX)-endPoint.w)>9 && Math.abs((flightPoint.h - screenY)-endPoint.h)>9)&&!server)//(flightPoint.w<=1000 && flightPoint.w>-1000) && (flightPoint.h<=1000 && flightPoint.h>-1000))
+            setTimeout((w: number, h: number, sX: number, sY: number, wi: number, he: number, startP: {w: number, h: number}, ang: number)=>{
+                const ySpeed = 10*Math.sin(ang);
+                const xSpeed = -10*Math.cos(ang);
+                setFlightPoint({w: (wi/2+sX)>(startP.w)?w-xSpeed:w+xSpeed, h: (wi/2+sX)<(startP.w)?h-ySpeed:h+ySpeed})
+            }, 10, flightPoint.w, flightPoint.h, screenX, screenY, width, height, startPoint, angle)
         let canvas: any = document.getElementById("canvas");
         
         if (canvas.getContext) {
             var ctx = canvas.getContext("2d");
-            ctx.clearRect(-100,100, width*2, height*2)
-            ctx.translate(width/2, height/2)
-            ctx.rotate(-aangle + lastAngle.current);//*Math.PI/180);
-            ctx.beginPath();
-            ctx.moveTo(-50, 0);
-            ctx.arc(0, 0, 50, 0, Math.PI * 2, true);
-            ctx.stroke();
-            ctx.beginPath();
-            if (flightPoint.w || flightPoint.h) ctx.arc(flightPoint.w, flightPoint.h, 10, 0, Math.PI * 2, true);
-            ctx.stroke();
-            ctx.translate(-width/2, -height/2);
-            lastAngle.current = (aangle||0);
+            ctx.clearRect(0, 0, width*2, height*2)
+            if (!server) {
+                ctx.beginPath();
+                ctx.arc(width/2, height/2, 20, 0, Math.PI * 2, true);
+                ctx.arc(flightPoint.w-screenX, flightPoint.h-screenY, 10, 0, Math.PI * 2, true);
+                ctx.stroke();
+            }
+
+            else {
+                ctx.beginPath();
+                ctx.moveTo(width/2-50*Math.cos(angle), height/2-50*Math.sin(angle));
+                ctx.lineTo(width/2+50*Math.cos(angle), height/2+50*Math.sin(angle));
+                ctx.stroke();
+                ctx.beginPath();
+                ctx.arc(width/2, height/2, 50, 0, Math.PI * 2, true);
+                ctx.stroke();
+                ctx.beginPath();
+                ctx.arc(flightPoint.w+width/2, flightPoint.h+height/2, 10, 0, Math.PI * 2, true);
+                ctx.stroke();
+            }
+            lastAngle.current = angle;
         }
-    }, [screenX, screenY, flightPoint, endPoint])
+    }, [angle, flightPoint, width, height, screenX, screenY])
 
     useEffect(()=>{
-        const x1 = window.screenX;
-        const y1 = window.screenY;
-        const aangle = Math.atan(((y1+window.innerHeight/2)-endPoint.h)/((x1+window.innerWidth/2)-endPoint.w))||0;
-        console.log(`W: ${width}, H: ${height}, X: ${screenX}, Y: ${screenY}, alpha: ${aangle*180/Math.PI}, last: ${lastAngle.current*180/Math.PI}`);
-        screenXRef.current = screenX;
-        screenYRef.current = screenY;
+        const ang = Math.atan(((window.screenY+window.innerHeight/2)-endPoint.h)/((window.screenX+window.innerWidth/2)-endPoint.w))||0
+        if (serverRef.current) setAngle(ang);
+        console.log(ang*180/3.14);
+        endPointRef.current = endPoint;
+    }, [endPoint])
 
-        if (flightPoint.w>=-17 && flightPoint.w<1000) 
-            setTimeout((w: number, h: number)=>{
-                setFlightPoint({w: w+5, h: h})
-            }, 10, flightPoint.w, flightPoint.h)
 
-        let canvas: any = document.getElementById("canvas");
-        
-        if (canvas.getContext) {
-            var ctx = canvas.getContext("2d");
-            ctx.clearRect(-100,100, width*2, height*2)
-            ctx.translate(width/2, height/2)
-            ctx.rotate(aangle);//*Math.PI/180);
-            ctx.beginPath();
-            ctx.moveTo(-50, 0);
-            ctx.arc(0, 0, 50, 0, Math.PI * 2, true);
-            ctx.stroke();
-            ctx.beginPath();
-            if (flightPoint.w || flightPoint.h) ctx.arc(flightPoint.w, flightPoint.h, 10, 0, Math.PI * 2, true);
-            ctx.stroke();
-            ctx.translate(-width/2, -height/2);
-            lastAngle.current = (aangle||0);
-        }
-    }, [width, height])
+    useEffect(()=>{
+        const ang = Math.atan(((window.screenY+window.innerHeight/2)-startPoint.h)/((window.screenX+window.innerWidth/2)-startPoint.w))||0
+        if (!serverRef.current) setAngle(ang);
+        console.log(ang*180/3.14);
+        startPointRef.current = startPoint;
+    }, [startPoint])
 
-    const pict = () => {
-        
-    }
+    useEffect(()=>{
+        serverRef.current = server
+    }, [server])
 
     return (
         <canvas id={'canvas'} width={width} height={height} style={{position: 'absolute', width: '100%', height: '100%', top: 0, left: 0, backgroundColor: 'aliceblue'}} />
