1f463563924199f3fa7e5fd8e0b338f29152fc85
pyshnenko <pyshnenko94@yandex.ru>
2024-06-05 15:33:12 +0300

    05/06/2024

--- src/frontDesign/filePalette.tsx
+++ src/frontDesign/filePalette.tsx
@@ -376,13 +376,13 @@ export default function FilePalette ({files, path, setSelectedId, selectedId, se
                                     fileType(item)==='picture'? 
                                         <Box sx={{width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
                                             <Box sx={{width: '60px', height: '60px'}}>
-                                                <img style={{width: '100%'}} src={`${window.location.href.includes('http://localhost:8799/')?'http://localhost:8800':''}/data/${path}/${item}`} />
+                                                <img style={{width: '100%', maxHeight: '100%'}} src={`${window.location.href.includes('http://localhost:8799/')?'http://localhost:8800':''}/data/${path}/${item}`} />
                                             </Box>
                                         </Box> :
                                     fileType(item)==='video'? 
                                     <Box sx={{width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
                                         <Box sx={{width: '60px', height: '60px'}}>
-                                            <video style={{width: '100%'}} src={`${window.location.href.includes('http://localhost:8799/')?'http://localhost:8800':''}/data/${path}/${item}`} autoPlay={selectedId===index+files.directs.length?true:false} muted={true} is="x-muted" loop={true} />
+                                            <video style={{width: '100%', maxHeight: '100%'}} src={`${window.location.href.includes('http://localhost:8799/')?'http://localhost:8800':''}/data/${path}/${item}`} autoPlay={selectedId===index+files.directs.length?true:false} muted={true} is="x-muted" loop={true} />
                                         </Box>
                                     </Box> :
                                     <InsertDriveFileIcon sx={{zoom: 2.5, color: '#0AD58D'}} />}
