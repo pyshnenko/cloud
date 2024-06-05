899dd574620d86bfca18fcb9522677f1c126e4f4
pyshnenko <pyshnenko94@yandex.ru>
2024-06-05 15:19:05 +0300

    05/06/2024

--- src/frontDesign/filePalette.tsx
+++ src/frontDesign/filePalette.tsx
@@ -382,7 +382,7 @@ export default function FilePalette ({files, path, setSelectedId, selectedId, se
                                     fileType(item)==='video'? 
                                     <Box sx={{width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
                                         <Box sx={{width: '60px', height: '60px'}}>
-                                            <video autoPlay={selectedId===index+files.directs.length} muted={true} controls={false} loop={true} style={{width: '100%'}} src={`${window.location.href.includes('http://localhost:8799/')?'http://localhost:8800':''}/data/${path}/${item}`} />
+                                            <video autoPlay={selectedId===index+files.directs.length?true:false} muted={true} is="x-muted" loop={true} style={{width: '100%'}} src={`${window.location.href.includes('http://localhost:8799/')?'http://localhost:8800':''}/data/${path}/${item}`} />
                                         </Box>
                                     </Box> :
                                     <InsertDriveFileIcon sx={{zoom: 2.5, color: '#0AD58D'}} />}
