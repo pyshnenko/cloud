8ca1b510971bb051550c471cfc9737634a610d27
pyshnenko <pyshnenko94@yandex.ru>
2024-07-01 16:36:19 +0300

    animate

--- pages/demo3.tsx
+++ pages/demo3.tsx
@@ -21,12 +21,23 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Typography from '@mui/material/Typography';
 import DemoAccordeon from '../src/frontDesign/small/demoAccordeon';
+import { SVGLoader } from 'three/addons/loaders/SVGLoader.js';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 
 const darkTheme = createTheme({
 	palette: {
 		mode: 'dark',
 	},
 });
+
+const scale = 0.025;
+
+const models: {name: string, x: number, y: number, z: number, rx: number, ry: number, rz: number}[] = [
+	{name: '/scel.glb', x: 0, y: 0, z: -0.6, rx: 0, ry: 0, rz: 0},
+	{name: '/PCB131.glb', x: -3.3, y: -0.85, z: -0.1, rx: 0, ry: 3.14, rz: 1.57},
+	{name: '/PCB2.glb', x: -3.05, y: -0.6, z: 0.35, rx: 0, ry: 0, rz: 0},
+	{name: '/PCB3.glb', x: -2.27, y: 1.16, z: 0.35, rx: 0, ry: 0, rz: 0}
+]
 
 const butts = [
 	{top: 30, left: false, name: 'Золотолесье', url: 'https://gf.spamigor.ru', text: 'Начальная стадия форума ролевиков с казной, событиями и т д'},	
@@ -41,10 +52,14 @@ const butts = [
 export default function NeonTest () {
 
     useEffect(()=>{
-        let camera: any, stats: any, model: any;
+        let camera: any, stats: any, model: any[] = [];
 			let composer: any, renderer: any, mixer: any, clock: any;
             let bloomPass: any;
 			let group: any = new THREE.Group();
+			const raycaster = new THREE.Raycaster();
+			let groupInp: any = new THREE.Group();
+
+			const scene = new THREE.Scene();
 
 			const params: any = {
 				threshold: 0,
@@ -60,28 +75,133 @@ export default function NeonTest () {
 				const container = document.getElementById( 'container' );
 
 				clock = new THREE.Clock();
-
-				const scene = new THREE.Scene();
-
+				scene.background = new THREE.Color(  0x292C2F );
 				camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 100 );
-				camera.position.set( 0, 0, -10 );//( - 5, 2.5, - 3.5 );
+				camera.position.set( 0, 0, -10 ); //( - 5, 2.5, - 3.5 );
 				scene.add( camera );
 
 				scene.add( new THREE.AmbientLight( 0xcccccc ) );
 
+				const light = new THREE.HemisphereLight( 0xffffff, 0x080820, 10 );
+				scene.add( light );
+
 				const pointLight = new THREE.PointLight( 0xffffff, 100 );
-				camera.add( pointLight );
+				//camera.add( pointLight );
+
+				const loaderF = new FontLoader();
+				loaderF.load( 'https://spamigor.ru/library/threejs/examples/fonts/helvetiker_regular.typeface.json', function ( font ) {
+
+					const color = new THREE.Color( 0x006699 );
+
+					const matDark = new THREE.MeshBasicMaterial( {
+						color: color,
+						side: THREE.DoubleSide
+					} );
+
+					const matLite = new THREE.MeshBasicMaterial( {
+						color: color,
+						transparent: true,
+						opacity: 0.4,
+						side: THREE.DoubleSide
+					} );
+
+					const message = '   Three.js\nStroke text.';
+
+					const shapes = font.generateShapes( message, 10 );
+
+					const geometryF = new THREE.ShapeGeometry( shapes );
+
+					geometryF.computeBoundingBox();
+
+					const xMid = - 0.05 * ( geometryF.boundingBox?.max.x||0 - (geometryF.boundingBox?.min.x||0) );
+
+					geometryF.translate( xMid, 0, 0 );
+
+					// make shape ( N.B. edge view not visible )
+
+					const text = new THREE.Mesh( geometryF, matLite );
+					text.position.z = - 150;
+					text.scale.x = scale;
+					text.scale.y = scale;
+					text.scale.z = scale;
+					scene.add( text );
+
+					// make line shape ( N.B. edge view remains visible )
+
+					/*const holeShapes: any[] = [];
+
+					for ( let i = 0; i < shapes.length; i ++ ) {
+
+						const shape = shapes[ i ];
+
+						if ( shape.holes && shape.holes.length > 0 ) {
+
+							for ( let j = 0; j < shape.holes.length; j ++ ) {
+
+								const hole = shape.holes[ j ];
+								holeShapes.push( hole );
+
+							}
+
+						}
+
+					}
+
+					shapes.push.apply( shapes, holeShapes );
+
+					const style = SVGLoader.getStrokeStyle( 5, color.getStyle() );
+
+					const strokeText = new THREE.Group();
+
+					for ( let i = 0; i < shapes.length; i ++ ) {
+
+						const shape = shapes[ i ];
+
+						const points = shape.getPoints();
+
+						const geometry = SVGLoader.pointsToStroke( points, style );
+
+						geometry.translate( xMid, 0, 0 );
+
+						const strokeMesh = new THREE.Mesh( geometry, matDark );
+						strokeText.add( strokeMesh );
+
+					}
+
+					scene.add( strokeText );*/
+
+				} );
+				
 
 				const loader = new GLTFLoader();
-				const gltf = await loader.loadAsync( '/ddl.glb' );//'models/gltf/PrimaryIonDrive.glb''https://spamigor.ru/library/threejs/examples/models/gltf/Parrot.glb'
+				for (let i = 0; i<models.length; i++) {
+					const gltf = await loader.loadAsync( models[i].name );//'models/gltf/PrimaryIonDrive.glb''https://spamigor.ru/library/threejs/examples/models/gltf/Parrot.glb'
+					
+					model[i] = gltf.scene;
+					model[i].position.y = models[i].y//-1 - PCB13  -1.51 -PCB3  -0.65 - PCB2  0 - scel
+					model[i].position.x = models[i].x//-1 - PCB13  -0.64 - PCB3  -0.63 - PCB2  0 - scel
+					model[i].position.z = models[i].z//0; // -0.6 scel
 
-				model = gltf.scene;
-                model.position.y = -2;
-                model.position.x = 1.25;
-                model.position.z = -1.25;
-                model.scale.set(0.025,0.025,0.025);
-				//scene.add( model );
-				group.add( model );
+					model[i].rotation.x = models[i].rx;
+					model[i].rotation.y = models[i].ry;
+					model[i].rotation.z = models[i].rz;
+
+					model[i].scale.set(scale,scale,scale);
+
+					model[i].name = models[i].name
+					//scene.add( model );
+
+					groupInp.add( model[i] );
+				}
+				console.log(groupInp);
+
+				/*const geometry = new THREE.BoxGeometry( 2*scale, 2*scale, 2*scale ); 
+				const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} ); 
+				const cube = new THREE.Mesh( geometry, material ); 
+				group.add( cube );*/
+				groupInp.position.x = 1.25;
+				groupInp.position.y = -2.25;
+				group.add(groupInp);
 				scene.add( group );
 
 				renderer = new THREE.WebGLRenderer( { antialias: true } );
@@ -112,9 +232,12 @@ export default function NeonTest () {
 				const controls = new OrbitControls( camera, renderer.domElement );
 				controls.maxPolarAngle = Math.PI * 1;
 				controls.minDistance = 3;
-				controls.maxDistance = 10;
+				controls.maxDistance = 20;
 
 				window.addEventListener( 'resize', onWindowResize );
+			
+				window.addEventListener( 'click', onMouseClick );
+				window.addEventListener("touchstart", onTouch);
 
 			}
 
@@ -131,11 +254,67 @@ export default function NeonTest () {
 
 			}
 
-			function animate() {
+			const mouse = new THREE.Vector2( 1, 1 );
 
+			function onTouch( event: TouchEvent ) {
+
+				event.preventDefault();
+				mouse.x = ( event.touches[0].clientX / window.innerWidth ) * 2 - 1;
+				mouse.y = - ( event.touches[0].clientY / window.innerHeight ) * 2 + 1;
+				click = true
+
+			}
+
+			function onMouseClick( event: MouseEvent ) {
+
+				event.preventDefault();
+
+				mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+				mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+				click = true
+
+			}
+
+			let click: boolean = true;
+			let moveId = {id: -1, sTime: 0, forw: true};
+
+			function animate() {
+				raycaster.setFromCamera( mouse, camera );
+				const intersection = raycaster.intersectObject( scene );
+				if (click) {
+					if (intersection.length) {
+						let dat: {id: number, name: string} | null = checkName(intersection[0].object);
+						if (dat && dat.id>0 && (Math.abs(groupInp.children[dat.id].position.x - models[dat.id].x)<1)) 
+							moveId = {id: dat.id, sTime: Number(new Date()), forw: true}
+						else if (dat && dat.id>0 && (Math.abs(groupInp.children[dat.id].position.x - models[dat.id].x))>2.9) 
+							moveId = {id: dat.id, sTime: Number(new Date()), forw: false}
+					}
+					click = false;
+				}
+				if ((moveId.id>=0)&&(moveId.id<groupInp.children.length)&&(moveId.sTime!==0)) {
+					let bool1: boolean = (Math.abs(groupInp.children[moveId.id].position.x - models[moveId.id].x)>=3)
+					if (bool1&&moveId.forw) {
+						groupInp.children[moveId.id].position.x = models[moveId.id].x + (moveId.id===1 ? 3: -3);
+						groupInp.children[moveId.id].position.z = models[moveId.id].z;
+						moveId={id: -1, sTime: 0, forw: true};
+					} else if (
+						(moveId.id===1 && !moveId.forw && groupInp.children[moveId.id].position.x<=models[moveId.id].x) ||
+						(moveId.id!==1 && !moveId.forw && groupInp.children[moveId.id].position.x>=models[moveId.id].x)
+					) {
+						groupInp.children[moveId.id].position.x = models[moveId.id].x;
+						groupInp.children[moveId.id].position.z = models[moveId.id].z;
+						moveId={id: -1, sTime: 0, forw: true};
+					}
+					else {
+						groupInp.children[moveId.id].position.x += ((moveId.id===1?1:-1)*(moveId.forw ? 3 : -3)/2000) * (Number(new Date()) - moveId.sTime);
+						let delta = Math.abs(groupInp.children[moveId.id].position.x - models[moveId.id].x);
+						groupInp.children[moveId.id].position.z = models[moveId.id].z + ((moveId.id!==1?1:-1) * Math.sin(Math.PI*delta/3))
+					}
+					
+				}
 				const delta = clock.getDelta();
 				let value = (Math.sin(Number(new Date())/1000)+1)/2;
-                bloomPass.strength = 0.1;//value;
+                bloomPass.strength = 0.15;//value;
 				group.rotation.y = 0//((Number(new Date())/1000)+1)/2;
 
 				//mixer.update( delta );
@@ -144,6 +323,15 @@ export default function NeonTest () {
 
 				composer.render();
 
+			}
+
+			function checkName (obj: any) {
+				if ((obj.hasOwnProperty('name'))||(obj.hasOwnProperty('parent'))) {
+					for (let i = 0; i< models.length; i++)
+						if (obj.name === models[i].name) return {name: obj.name, id: i}
+					return checkName(obj.parent)
+				}
+				else return null
 			}
     }, [])
 
