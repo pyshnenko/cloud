acb3ba44df1293fabb16292582da808adfa06fdc
pyshnenko <pyshnenko94@yandex.ru>
2024-09-29 12:48:14 +0300

    add test

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -4,6 +4,6 @@ const socketUrl = 'http://localhost:8801/socket';
 export default async function handler(req: any, res: any) {
 
     console.log(req.query);
-    axios.post(socketUrl, {text: `sim800l connected\r\n${req.query.a}`})
+    axios.post(socketUrl, {text: `sim800l connected\r\n${req.query.a}\r\nsms: ${req.query.sms}`})
     res.status(200).json({res: new Date().toLocaleString()});
 }
