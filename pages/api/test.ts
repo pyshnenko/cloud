eba0a7a39c62ee914cac40e044ba662baa4c1d9c
pyshnenko <pyshnenko94@yandex.ru>
2024-09-29 12:02:29 +0300

    add test

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,5 +1,5 @@
 export default async function handler(req: any, res: any) {
 
     console.log(req.query);
-    res.status(200).json({res: 'ok'});
+    res.status(200).json({res: new Date().toLocaleString()});
 }
