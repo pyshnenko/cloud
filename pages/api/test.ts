59278de4fc4fe0924ff5875dab2e5616006e61a1
pyshnenko <pyshnenko94@yandex.ru>
2025-02-20 10:40:44 +0300

    add cookies

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -11,13 +11,14 @@ interface CooKieToken {
 
 export default async function handler(req: NextRequest, res: NextApiResponse) {
     console.log('test');
-    let ccc: string = 'no token';
+    let token: string = 'no token';
     if (req?.cookies) {
         let ccc: CooKieToken = (req.cookies as any)
+        token = ccc.token || 'no token';
         console.log(ccc.token)
     }
     else console.log('no cookies');
     //axios.post(socketUrl, {text: `sim800l connected\r\n${req.query.a}\r\nsms: ${req.query.sms}`})
     console.log('res')
-    res.status(200).json({res: new Date().toLocaleString(), cookies: ccc});
+    res.status(200).json({res: new Date().toLocaleString(), cookies: token});
 }
